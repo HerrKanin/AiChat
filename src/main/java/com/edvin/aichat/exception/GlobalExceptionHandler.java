e57118c9bc45946fd3aa02c35bae11a6701c2fb7
Edvin Karlsson <edvinkarlsson4@gmail.com>
2026-05-05 21:02:45 +0200

    Add global exception handler

---
+++ src/main/java/com/edvin/aichat/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.edvin.aichat.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity
+                .internalServerError()
+                .body("Something went wring: " + e.getMessage());
+    }
+
+}
